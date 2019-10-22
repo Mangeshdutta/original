34def28e2915abba63ac3118ed3280fdda919670
Yunus Emre Deligöz <ye@deligoz.me>
2019-10-22 23:15:05 +0300

    Add Relationship Types in Category Model

--- app/Category.php
+++ app/Category.php
@@ -3,17 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Category extends Model
 {
     protected $fillable = ['parent_id', 'name'];
 
-    public function children()
+    public function children(): HasMany
     {
         return $this->hasMany(Category::class, 'parent_id', 'id');
     }
 
-    public function parent()
+    public function parent(): BelongsTo
     {
         return $this->belongsTo(Category::class, 'id', 'parent_id');
     }
