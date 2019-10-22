3be7fdac84b5012f056f93ef18cc3e0c6d612133
Yunus Emre Deligöz <ye@deligoz.me>
2019-10-22 23:24:30 +0300

    Add $fillables to Show Model

--- app/Show.php
+++ app/Show.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Show extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'slug', 'description',
+        'short_description', 'show_type',
+        'explicit_content', 'tags', 'author',
+        'owner', 'owner_email', 'copyright', 'website',
+        'language_id', 'time_zone_id', 'artwork', 'itunes_url',
+        'spotify_url',
+    ];
 }
