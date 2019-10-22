74d5cf1778fe07c73ef59762c3e1db59b96a6058
Yunus Emre Deligöz <ye@deligoz.me>
2019-10-22 23:25:10 +0300

    Cast 'show_type' to Enum in Show Model

--- app/Show.php
+++ app/Show.php
@@ -2,10 +2,14 @@
 
 namespace App;
 
+use App\Enums\ShowType;
+use BenSampo\Enum\Traits\CastsEnums;
 use Illuminate\Database\Eloquent\Model;
 
 class Show extends Model
 {
+    use CastsEnums;
+
     protected $fillable = [
         'title', 'slug', 'description',
         'short_description', 'show_type',
@@ -13,5 +17,13 @@ class Show extends Model
         'owner', 'owner_email', 'copyright', 'website',
         'language_id', 'time_zone_id', 'artwork', 'itunes_url',
         'spotify_url',
+    ];
+
+    protected $casts = [
+        'show_type' => 'int',
+    ];
+
+    protected $enumCasts = [
+        'show_type' => ShowType::class,
     ];
 }
